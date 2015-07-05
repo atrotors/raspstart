3e2768634f74da1b96c3d04c4bb9a2f9d2b258e5
atrotors <dev@atrotors.com>
2015-07-05 06:12:26 -0400

    Socket.io server now remembers power led state and emits on connection

--- socket/index.js
+++ socket/index.js
@@ -2,12 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var state;
+
 io.on('connection', function(socket){
+  if(state)
+    socket.emit('LED', state);
+
   socket.on('power_button', function(status){
     if (status == 'true') {
       io.emit('LED', 'on');
+      state = 'on';
     } else if (status == 'false') {
       io.emit('LED', 'off');
+      state = 'off';
     }
   });
 });
